26b60f2ecbac62eced8ad2cf0ebd31de4ba9c25d
Adam G <25728719+Electromaster232@users.noreply.github.com>
2018-02-17 12:34:15 -0500

    Create song.js

---
+++ song.js
@@ -0,0 +1,7 @@
+function pickSong() {
+    var song = ["https://www.youtube.com/embed/0yVWm6KIyMI?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&iv_load_policy=3", "https://www.youtube.com/embed/SswdHwQu2wU?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&iv_load_policy=3", "https://www.youtube.com/embed/kxopViU98Xo?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&iv_load_policy=3"];
+
+    var random = Math.floor(Math.random() * (song.length - 1));
+
+    document.getElementById("songURL").setAttribute("src", song[random]);
+}
