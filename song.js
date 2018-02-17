8430ff13f6d974ca7cb8da0934993b079f3fa903
Adam G <25728719+Electromaster232@users.noreply.github.com>
2018-02-17 12:38:27 -0500

    Update song.js

--- song.js
+++ song.js
@@ -1,4 +1,4 @@
-window.onload = function pickSong() {
+function pickSong() {
     var song = ["https://www.youtube.com/embed/0yVWm6KIyMI?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&iv_load_policy=3", "https://www.youtube.com/embed/SswdHwQu2wU?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&iv_load_policy=3", "https://www.youtube.com/embed/kxopViU98Xo?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&iv_load_policy=3"];
 
     var random = Math.floor(Math.random() * (song.length - 1));
